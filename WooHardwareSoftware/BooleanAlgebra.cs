ef98211153879590016408dccc96da1d7ed9fc25
Jon Woo <jw56578@gmail.com>
2016-06-04 22:02:06 -0500

    Add Encoding

--- WooHardwareSoftware/BooleanAlgebra.cs
+++ WooHardwareSoftware/BooleanAlgebra.cs
@@ -20,14 +20,79 @@ namespace WooHardwareSoftware
             b = (byte)~a; //flipped to 11111111, 256 decimal
             a = (byte)0x69; //01101001, 105 decimal
             b = (byte)0x55;// 01010101, 85 decimal
-            c = (byte)(a & b);//this takes the above bits and does an AND on each "column"
+            c = (byte)(a & b);//this takes the above bits and does an AND comparison on each "column"
             //0&&0 = 0, 1 &&1 =1, ,1 &&0 = 0....etc
             //65 decimal, 01000001
         }
         [TestMethod]
-        public void LogicalOperrationTests()
+        public void LogicalOperationTests()
         {
+            //lower level languages allow logic comparisons on bytes, integers..etc
+            //this is like javascript falsey truthy
+            //anything that is not a 0 is true
+            //but you cannot do this in c# 
+            //you can mimic it by doing a convert or parse
+            //anything but a 0 will get converted to true
+            byte a = 0x41;
+            bool boolA = Convert.ToBoolean(a);
+            bool notBoolA = !boolA;
+
+            byte b = 0x00;
+            bool boolB = Convert.ToBoolean(b);
+            bool notBoolB = !boolA;
+
 
         }
-   }
+        [TestMethod]
+        public void LogicalSetsTests()
+        {
+            //binary literals are not possible in C#
+            //use convert to mimic
+            
+            //each position in the byte represents a number according to position
+            //7,6,5,4,3,2,1,0
+            //wherever there is a 1 means the number is in the set
+            //if there is a zero it means the number is not in the set
+            //so this set is {0,3,5,6}
+            //because in the 1 position there is a 1
+            //in the 3 position there is a 1 
+            //in the 5 position there is a 1
+            //in the 6 position there is a 1
+            
+            byte a = Convert.ToByte("01101001",2); 
+
+            //{0,2,4,6}
+            byte b = Convert.ToByte("01010101", 2);
+
+            //& represents whatetver is in both sets
+            // 6 and 0
+            //01000001
+            var both = a & b;
+            var intersection = Convert.ToString(both, 2).PadLeft(8, '0');
+            Assert.AreEqual(intersection, "01000001");
+
+            //| represents all number in both sets, one time
+            //0,2,3,4,5,6
+            //01111101
+            var all = a | b;
+            var union = Convert.ToString(all, 2).PadLeft(8, '0');
+            Assert.AreEqual(union, "01111101");
+
+            //^ represents represents the number that are only in one set, not both
+            //2,3,4,5
+            //00111100
+            var either = a ^ b;
+            var symmetric = Convert.ToString(either, 2).PadLeft(8, '0');
+            Assert.AreEqual(symmetric, "00111100");
+
+            //~ applies to just one set and is just all the bits flipped
+            //if you don't cast it to byte it will be the same number but negative
+            var opposite = (byte)~b;
+            var compliment = Convert.ToString(opposite, 2).PadLeft(8, '0');
+            Assert.AreEqual(compliment, "10101010");
+
+
+
+        }
+    }
 }
---
+++ WooHardwareSoftware/Encoding.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WooHardwareSoftware
+{
+    /// <summary>
+    /// how would you represent one card from a deck in memory
+    /// there are 52 cards with a number value and a suit
+    /// </summary>
+    [TestClass]
+    public class Encoding
+    {
+        long bytesUsedOneHot = 428;
+        long bytesUsedTwoHot = 520;
+        /// <summary>
+        /// with this technique you would just use a 64 bit address space
+        /// you would use 52 bits of that space to represent each card
+        /// so there are 52 cards so you need 52 * 64 spaces of memory
+        /// each block of memory will have 1 in a different location to represent
+        ///     which of the 52 cards it is
+        /// ace of diamonds = 00000000000...0001
+        /// 2 of diamonds =   00000000000...0010
+        /// ....etc
+        /// king of spades =  10000000000...0000
+        /// this is a waste of memory
+        /// also it would be very hard to compare values and suits
+        /// you would have to make an if statement for every single card
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void OneHotEnodingTest()
+        {
+            var mem = GC.GetTotalMemory(true);
+
+            Int64[] deck = new long[52];
+            for (int i = 0; i < 52; i++)
+            {
+                deck[i] = i;
+            }
+            var memafter = GC.GetTotalMemory(true);
+            var memadded = memafter - mem;
+            Assert.AreEqual(memadded, bytesUsedOneHot);
+            var length = deck.Length;
+            Console.Write(length);
+            
+        }
+
+        /// <summary>
+        /// use 17 bits of 64 bits
+        /// 4 bits for the suit, 13 bits for the value
+        /// this wastes the same amount of memory
+        /// all it would do is make comparing things easier
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void TwoHotEnodingTest()
+        {
+            var mem = GC.GetTotalMemory(true);
+
+            Int64[] deck = new long[52];
+            string[] suits = new string[] {"0001","0010","0100","1000" };
+            string[] values = new string[] { "0000000000001", "0000000000010", "0000000000100", "0000000001000",
+            "0000000010000", "0000000100000", "0000001000000", "0000010000000",
+            "0000100000000", "0001000000000", "0010000000000", "0100000000000",
+            "1000000000000"};
+
+            int counter = 0;
+            foreach (var suit in suits)
+            {
+                foreach (var val in values)
+                {
+                    long a = Convert.ToInt64("00000000000000000000000000000000000000000000000" + suit + val, 2);
+                    deck[counter] = a;
+                    counter++;
+                }
+            }
+            suits = null;
+            values = null;
+            var memafter = GC.GetTotalMemory(true);
+            var memadded = memafter - mem;
+            Assert.AreEqual(memadded, bytesUsedTwoHot);
+            var length = deck.Length;
+            Console.Write(length);
+        }
+    }
+}
